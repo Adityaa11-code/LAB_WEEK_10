647aabcf5364cd051ae124d4d0203460f8bf9a41
Adityaa11-code <winstrek212@gmail.com>
2025-11-24 12:44:26 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/FirstFragment.kt
+++ app/src/main/java/com/example/lab_week_10/FirstFragment.kt
@@ -4,34 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.databinding.FragmentFirstBinding
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class FirstFragment : Fragment() {
 
-    private lateinit var textTotalFragment: TextView
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
-    private val viewModel: TotalViewModel by lazy {
-        ViewModelProvider(requireActivity())[TotalViewModel::class.java]
-    }
+    private lateinit var viewModel: TotalViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_first, container, false)
-        textTotalFragment = view.findViewById(R.id.text_total_fragment)
-        updateTotalText(viewModel.total)
-        return view
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    fun refresh() {
-        updateTotalText(viewModel.total)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Get ViewModel dari Activity
+        viewModel = ViewModelProvider(requireActivity())[TotalViewModel::class.java]
+
+        // Observe LiveData
+        viewModel.total.observe(viewLifecycleOwner) { total ->
+            binding.textFragment.text = "Fragment Total: $total"
+        }
     }
 
-    private fun updateTotalText(value: Int) {
-        textTotalFragment.text = "Total: $value"
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
